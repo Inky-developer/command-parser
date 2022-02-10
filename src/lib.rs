1e086a35d973f4dcec407da68f3597817422733e
SuperTails <sciencedude2003@gmail.com>
2022-02-10 12:28:55 -0700

    Fixed parsing of integers with adjacent symbols

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,13 @@ pub fn parse_str(value: &str) -> (&str, &str) {
 
 impl CommandParse for i32 {
     fn parse_from_command(value: &str) -> Result<(&str, Self), &str> {
-        let (rest, value_str) = parse_str(value);
+        let end_idx = if let Some(value_neg) = value.strip_prefix('-') {
+            value_neg.find(|c: char| !c.is_digit(10)).unwrap_or(value_neg.len()) + 1
+        } else {
+            value.find(|c: char| !c.is_digit(10)).unwrap_or(value.len())
+        };
+
+        let (value_str, rest) = value.split_at(end_idx);
         let value = value_str.parse().map_err(|_| value)?;
         Ok((rest, value))
     }
@@ -59,6 +65,7 @@ impl CommandParse for i32 {
 
 impl CommandParse for f32 {
     fn parse_from_command(value: &str) -> Result<(&str, Self), &str> {
+        // TODO: This will break on comma-separated lists
         let (rest, value_str) = parse_str(value);
         let value = value_str.parse().map_err(|_| value)?;
         Ok((rest, value))
@@ -67,6 +74,7 @@ impl CommandParse for f32 {
 
 impl CommandParse for f64 {
     fn parse_from_command(value: &str) -> Result<(&str, Self), &str> {
+        // TODO: This will break on comma-separated lists
         let (rest, value_str) = parse_str(value);
         let value = value_str.parse().map_err(|_| value)?;
         Ok((rest, value))
@@ -90,5 +98,19 @@ where
     fn parse_from_command(value: &str) -> Result<(&str, Self), &str> {
         let (rest, value) = T::parse_from_command(value)?;
         Ok((rest, Box::new(value)))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::CommandParse;
+
+    #[test]
+    fn test_i32() {
+        assert_eq!(i32::parse_from_command("123"), Ok(("", 123)));
+        assert_eq!(i32::parse_from_command("-123"), Ok(("", -123)));
+        assert_eq!(i32::parse_from_command("17 34"), Ok((" 34", 17)));
+        assert_eq!(i32::parse_from_command("17, 34"), Ok((", 34", 17)));
+        assert_eq!(i32::parse_from_command("abcd 123"), Err("abcd 123"));
     }
 }
